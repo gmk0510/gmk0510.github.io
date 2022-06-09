6970b7a2617a02287c2585f947a8bc1a0aa4b39d
gmk0510 <mingu.ku@gmail.com>
2022-06-10 00:23:04 +0900

    d

---
+++ 6-vue/src/components/TodoInput.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="ture"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem:''
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodoItem !== '') { // newTodoItem 의 값이 공백이 않을 경우
+                var value = this.newTodoItem && this.newTodoItem.trim();
+                this.$emit('addTodo', value);
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = ''
+        }
+    }
+}
+</script>
+
+<style>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: #fff;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: .9rem;
+    }
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+        cursor: pointer;
+    }
+    .addBtn {
+        color: #fff;
+        vertical-align: middle;
+    }
+</style>
---
+++ 6-vue/src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="(todoItem, index) in propsdata" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="ture"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        removeTodo(todoItem, index) {
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: .5rem 0;
+        padding: 0 .9rem;
+        background: #fff;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-left: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: red;
+    }
+</style>
